444b5aadea8422d0840388e5b610962afd775ba0
ksw2638 <42162177+ksw2638@users.noreply.github.com>
2019-03-24 18:13:13 +0900

    Add files via upload
    send my room temperature data to thingspeak server.

---
+++ temperature2.ino
@@ -0,0 +1,89 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS D4
+// to be changed
+// the number that you connect with ds18b20
+// the number can be activated is case by case
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+String apiKey = "ZLY8FIS5MJLD2CI0"; // to be changed
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // to be changed
+  // We start by connecting to a WiFi network
+  // Wifi name and the password
+  WiFiMulti.addAP("ELEXION", "");
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Wait for WiFi... ");
+
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  delay(500);
+  Serial.println("Dallas Temperature");
+  sensors.begin();
+}
+
+void senddata(int flag) {//implementing double connection in loop function was error
+  WiFiClient client;
+  sensors.requestTemperatures();
+  Serial.print("Temperature for Device 1 is: ");
+  String temp = String(sensors.getTempCByIndex(0));
+  Serial.println(temp);
+  delay(500);
+  const char * host; // ip or dns
+  String url;
+  uint16_t port;
+
+  if(flag==0){// sending data to thingspeak
+    host="api.thingspeak.com";
+    port = 80;
+    url = "/update?api_key=" + apiKey + "&field1=" + temp;
+  }
+  if(flag==1){// sending data to my server.
+      host = "3.16.76.86";//public ip of your server
+      port = 8000; // over 1000
+      url = "/log?temp=" + temp; 
+  }
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  if(flag==1) {
+    host = "http://3.16.76.86";// public ip of your server
+  }
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  Serial.println(host);
+  delay(1000); // should request after 15 seconds
+}
+
+void loop() {
+  // make account on thingspeak
+  // write apikey of channel
+
+  static long millisSec = millis();
+
+  if (millis() - millisSec > 60000) { // time interval is 1 minute
+    millisSec = millis();
+    senddata(0);
+
+    senddata(1);
+  }
+}
